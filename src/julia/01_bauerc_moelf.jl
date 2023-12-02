baff9801072dc2fc11cd1c5bc9145db1eea32461
Moelf <proton@jling.dev>
2023-12-02 11:50:01 +0100

    add Day2 for Julia

--- src/julia/01_bauerc_moelf.jl
+++ src/julia/01_bauerc_moelf.jl
@@ -4,7 +4,7 @@ const INPUT_PATH = ARGS[1]
 function getnumber(line)
     i = findfirst(isdigit, line)
     j = findlast(isdigit, line)
-    return parse(Int, string(line[i],line[j]))
+    return parse(Int, string(line[i], line[j]))
 end
 
 function part1()
@@ -14,13 +14,23 @@ end
 
 part1() # Answer: 55123
 
-const lookup = Dict("one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine" => 9)
+const lookup = Dict(
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9,
+)
 
 const RE = Regex("$(join(keys(lookup), '|'))|\\d")
 
 # Part II
 function get_num_from_string(str)
-	dig = tryparse(Int, str)
+    dig = tryparse(Int, str)
     !isnothing(dig) && return dig
     return lookup[str]
 end
@@ -28,7 +38,7 @@ end
 function getnumber_part2(line)
     # overlap=true is important
     # getnumber_part2("6vzpjhtwonemc") == 61
-    captures = collect(eachmatch(RE, line; overlap=true))
+    captures = collect(eachmatch(RE, line; overlap = true))
     num_first = get_num_from_string(captures[begin].match)
     num_last = get_num_from_string(captures[end].match)
     return num_first * 10 + num_last
---
+++ src/julia/02_moelf.jl
@@ -0,0 +1,31 @@
+const INPUT_PATH = ARGS[1]
+const ALL_LINES = readlines(INPUT_PATH)
+
+function color_dict(a::RegexMatch)
+    num, color = a.captures
+    Dict(color => parse(Int, num))
+end
+
+function line_max(line)
+    RE = r"(\d+)\s(blue|green|red)"
+    parts = eachmatch(RE, line)
+    mapreduce(color_dict, mergewith(max), parts)
+end
+
+# Part I
+function part1_pred(line)
+    dict = line_max(line)
+    return dict["red"] <= 12 &&
+    dict["green"] <= 13 &&
+    dict["blue"] <= 14
+end
+
+p1 = sum(enumerate(ALL_LINES)) do (i, line)
+    return part1_pred(line) ? i : 0
+end
+println(p1)
+
+# Part II
+part2(line) = line |> line_max |> values |> prod
+p2 = sum(part2, ALL_LINES)
+println(p2)
