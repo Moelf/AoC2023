2a8c995416a52e667575ca3f71b5cd54cafe443b
Jerry Ling <proton@jling.dev>
2023-12-04 23:01:58 +0100

    Realod input paths every time (#28)

--- runtests.jl
+++ runtests.jl
@@ -5,10 +5,12 @@ Pkg.activate(@__DIR__)
 using AoC2023
 
 const cookie = get(ENV, "AOC_COOKIE", "")
+if get(ENV, "CI", "false") == "true"
+    isempty(cookie) && error("AOC_COOKIE environment variable must be set for CI")
+end
 const HEADERS = headers=Dict("Cookie" => "session=$cookie")
 
 const ALL_LANGUAGES = isempty(ARGS) ? ["julia","cpp","python"] : ARGS
-const INPUTS = readdir(joinpath(@__DIR__, "inputs"))
 
 """
     run_solution(source_path, input_path, ::Val{T}) -> Vector{String}
@@ -43,7 +45,8 @@ function run_solution(source_path, input_path, ::Val{:python})
 end
 
 function get_all_inputs_solutions(day_num_str)
-    paths = filter(startswith(day_num_str), INPUTS)
+    input_paths = readdir(joinpath(@__DIR__, "inputs"))
+    paths = filter(startswith(day_num_str), input_paths)
     pairs = map(paths) do p
         input = joinpath("./inputs", p)
         solution = joinpath("./solutions", p)
