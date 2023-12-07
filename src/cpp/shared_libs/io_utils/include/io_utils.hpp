d190a36017c0efd27d5dfeb9ca46bd6219891448
Xu Ruoyang <ruoyangx@gmail.com>
2023-12-07 13:31:05 -0500

    Day 6 (#32)

--- src/cpp/shared_libs/io_utils/include/io_utils.hpp
+++ src/cpp/shared_libs/io_utils/include/io_utils.hpp
@@ -3,6 +3,9 @@
 #include <fstream>
 #include <filesystem>
 #include <vector>
+#include <regex>
+#include <type_traits>
+#include <iostream>
 
 namespace io_utils {
 
@@ -10,5 +13,19 @@ namespace fs = std::filesystem;
 
 // Parse input text files into vector of strings separated by line
 std::vector<std::string> parseInputFile(const fs::path& path);
+
+template <typename T = std::string>
+T stringToNumber(const std::string & arg)
+{
+    if (std::is_same_v<T, int>) {
+        return std::stoi(arg);
+    } else if (std::is_same_v<T, long>) {
+        return std::stol(arg);
+    } else {
+        return T();
+    }
+}
+
+std::vector<std::string> splitString(const std::string & line, const std::string && delim);
 
 } // io_utils
