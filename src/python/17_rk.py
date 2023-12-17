9abc532d35b8bc8edb9c9d6e9e9e4f4199840fe9
Rongkun Wang <elviswang011@gmail.com>
2023-12-17 19:28:27 +0100

    Dijkstra's algorithm day17

--- src/python/17_rk.py
+++ src/python/17_rk.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
 import collections
+import math
+from heapq import heappop, heappush
 
 def parse():
     l = []
@@ -51,15 +53,17 @@ def sol_first(l, sol1 = True):
                 dq.append((r+dr, c+dc, dr, dc, step+1, loss))
             # turn left/right
             # if going right or down, consider go up or left
-            if dc > 0 or dr > 0:
-                dq.append((r+dc, c+dr, dc, dr, 1, loss))
-                dq.append((r-dc, c-dr, -dc, -dr, 1, loss))
+            dq.append((r+dc, c+dr, dc, dr, 1, loss))
+            dq.append((r-dc, c-dr, -dc, -dr, 1, loss))
+            #  if dc > 0 or dr > 0:
+                #  dq.append((r+dc, c+dr, dc, dr, 1, loss))
+                #  dq.append((r-dc, c-dr, -dc, -dr, 1, loss))
             # if going left, only consider go down
-            if dc < 0:
-                dq.append((r+1, c, 1, 0, 1, loss))
+            #  if dc < 0:
+                #  dq.append((r+1, c, 1, 0, 1, loss))
             # if going up, only consider go right
-            if dr < 0:
-                dq.append((r, c+1, 0, 1, 1, loss))
+            #  if dr < 0:
+                #  dq.append((r, c+1, 0, 1, 1, loss))
         else:
             # go straight is a possibility  when steps < 10
             if step < 10:
@@ -67,15 +71,8 @@ def sol_first(l, sol1 = True):
 
             if step >= 4:
                 # go left and right only when step is >=4
-                if dc > 0 or dr > 0:
-                    dq.append((r+dc, c+dr, dc, dr, 1, loss))
-                    dq.append((r-dc, c-dr, -dc, -dr, 1, loss))
-                # if going left, only consider go down
-                if dc < 0:
-                    dq.append((r+1, c, 1, 0, 1, loss))
-                # if going up, only consider go right
-                if dr < 0:
-                    dq.append((r, c+1, 0, 1, 1, loss))
+                dq.append((r+dc, c+dr, dc, dr, 1, loss))
+                dq.append((r-dc, c-dr, -dc, -dr, 1, loss))
             pass
         pass
 
@@ -83,10 +80,67 @@ def sol_first(l, sol1 = True):
     
     return min([l for ID, l in mhl.items() if ID[0] == height - 1 and ID[1] == width - 1])
 
+
+def is_valid(height, width, r, c):
+    return r >= 0 and c >= 0 and r < height and c < width
+
+def sol_second_weighted_consideration(l, sol1 = True):
+    height, width = dim(l)
+    # min heat loss of ID=(pos row, pos col, direction row, direction col, step) : loss
+    # the direction is how you enter this tile
+    mhl = {}
+    dq = []
+    # ID of five number, last is the loss before entering
+    heappush(dq, (0, (0, 1, 0, 1, 1)) )
+    heappush(dq, (0, (1, 0, 1, 0, 1)) )
+    # need to know the min loss from all 3!
+    while len(dq):
+        loss, ID = heappop(dq)
+        r, c, dr, dc, step = ID
+
+        loss += l[r][c]
+        if loss >= mhl.get(ID, math.inf):
+                continue
+
+        # cache first
+        mhl[ID] = loss
+
+        max_step = 3
+        min_step = 0
+        if not sol1:
+            max_step = 10
+            min_step = 4
+            pass
+
+        # direction and next step
+        possible_direct = []
+        # go straight
+        if step < max_step:
+            possible_direct.append((dr, dc, step+1))
+
+        # go left right
+        if step >= min_step:
+            possible_direct.append((dc, dr, 1))
+            possible_direct.append((-dc, -dr, 1))
+        
+        for dr_test, dc_test, step_test in possible_direct:
+            newr = r + dr_test
+            newc = c + dc_test
+            #  ID = (r, c, dr, dc, step)
+            if is_valid(width, height, newr, newc):
+                heappush(dq, (loss, (newr, newc, dr_test, dc_test, step_test)))
+            pass
+        
+        pass
+    
+    return min([l for ID, l in mhl.items() if ID[0] == height - 1 and ID[1] == width - 1])
+
 def main():
     l = parse()
-    print(sol_first(l))
-    print(sol_first(l, False))
+    #  print(sol_first(l))
+    #  print(sol_first(l, False))
+    print(sol_second_weighted_consideration(l))
+    print(sol_second_weighted_consideration(l, False))
     pass
 
 main()
